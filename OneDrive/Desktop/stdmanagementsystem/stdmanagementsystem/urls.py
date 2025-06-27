c3ab1054b42cedb8535dcb95a53a820af46b8c67
Ian Otieno <otienoian229@gmail.com>
2025-06-27 14:39:25 +0300

    Delete OneDrive/Desktop/stdmanagementsystem directory

--- OneDrive/Desktop/stdmanagementsystem/stdmanagementsystem/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stdapp.urls')),  # Ensure 'myapp' is replaced with the name of your app
-]
